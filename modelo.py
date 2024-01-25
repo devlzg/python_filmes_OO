f2ec8594514ceb8f5d47619e2342265efcf4ebcb
Luiz Gabriel <lzg.lisboasantos5569@gmail.com>
2024-01-25 18:16:47 -0300

    Add files via upload

--- modelo.py
+++ modelo.py
@@ -1,26 +1,47 @@
-class Filme:
-    def __init__(self, nome, ano, duracao) -> None:
-        self.nome = nome.title()
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
         self.ano = ano
+        self._likes = 0
+        
+    @property
+    def likes(self):
+        return self._likes
+    
+    def dar_like(self):
+        self._likes += 1
+    
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.likes = 0
-    
-    def dar_like(self):
-        self.likes += 1
-    
+    def __str__(self) -> str:
+        return f"{self.nome} - {self.ano} - {self.duracao} min - {self.likes} Likes"
 
-class Serie:
-    def __init__(self, nome, ano, temporadas) -> None:
-        self.nome = nome.title()
-        self.ano = ano
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.likes = 0
+    
+    def __str__(self) -> str:
+        return f"{self.nome} - {self.ano} - {self.temporadas} temporadas - {self.likes} Likes"
 
-    def dar_like(self):
-        self.likes += 1
+
 vingadores = Filme("Vingadores - Guerra Infinita", 2018, 160)
-print (f"Nome : {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}")
-
+vingadores.dar_like()
 atlanta = Serie("Atlanta", 2018, 2)
+atlanta.dar_like()
+atlanta.dar_like()
 
-print (f"Nome : {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}")
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    print(programa)
